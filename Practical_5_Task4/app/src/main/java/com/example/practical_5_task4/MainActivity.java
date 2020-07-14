d22cc36764c2358eb901f37a5581b29dd985cee6
yapzhengyew <yapzy-wm19@student.tarc.edu.my>
2020-07-14 16:43:24 +0800

    Practical 5 Task 4

---
+++ Practical_5_Task4/app/src/main/java/com/example/practical_5_task4/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.practical_5_task4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickShowAlert(View view) {
+        AlertDialog.Builder myAlertBuilder = new AlertDialog.Builder(MainActivity.this);
+        // Set the dialog title and message.
+        myAlertBuilder.setTitle("Alert");
+        myAlertBuilder.setMessage("Click OK to continue or Cancel to stop.");
+        // Add the dialog buttons.
+        myAlertBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "Pressed OK", Toast.LENGTH_SHORT).show();
+            }
+        });
+        myAlertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "Pressed Cancel", Toast.LENGTH_SHORT).show();
+            }
+        });
+        // Create and show the AlertDialog.
+        myAlertBuilder.show();
+    }
+
+}
